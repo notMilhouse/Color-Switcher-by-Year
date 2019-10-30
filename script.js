701977ace4d7c67636137bbd77803002fdd435bf
Gabriel Carvalho Silva <gabrielwcs@hotmail.com>
2019-10-30 12:04:18 -0300

    Now changes apply to every item in the ui
    Every HTML element that have "dynamic-ui" class will change color according to the year or input

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ function setYear(){
     var respectiveColor = year % 7;
     document.getElementById("year").innerHTML = year;
     document.getElementById("box").style.background = colorList[respectiveColor];
+
+    const items = document.getElementsByClassName("dynamic-ui");
+    for(i = 0; i < items.length; i++){
+        items[i].style.background = colorList[respectiveColor];
+    }
 }
 
 var colorList = ["#EFD567","#A44878","#3047A3","#FFAC13","#188734","#28C9FA","#D4433A"];
 var year = new Date().getFullYear(); //current year
-var respectiveColor = year % 7;
+var respectiveColor = year % 7; //the denominator must be equal to the number of elements of the array
 
 document.getElementById("year").innerHTML = year;
 document.getElementById("box").style.background = colorList[respectiveColor];
+
+const items = document.getElementsByClassName("dynamic-ui");
+for(i = 0; i < items.length; i++){
+    items[i].style.background = colorList[respectiveColor];
+}
